326887343473f0e66b42151516fde4c781e10a3a
Leonel <leonel.carrossoni@gmail.com>
2020-11-11 16:22:53 -0300

    Inclusão formulário empresas

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -18,7 +18,7 @@ const Select: React.FC<SelectProps> = ({label, name, options, ...rest }) => {
         <div className="select-block">
             <label htmlFor= {name}> {label}</label>
             <select id={name} {...rest}>
-                <option value="" disabled selected hidden>Choose an option</option>
+                <option value="" disabled selected hidden >Choose an option</option>
                 {options.map(option => {
                     return <option key={option.value} value={option.value}>{option.label}</option>
                 })}
--- web/src/pages/FindWorker/index.tsx
+++ web/src/pages/FindWorker/index.tsx
@@ -1,13 +1,46 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Textarea from '../../components/Textarea';
 
 import './styles.css';
 
+import warningIcon from '../../assets/images/icons/warning.svg'
+
+
 function FindWorker() {
     return(
-        <div id="page-teacher-list" className="container">
-            <PageHeader title="Awesome that you need a worker." />
+        <div id="page-teacher-form" className="container">
+            <PageHeader 
+            title="Awesome that you need a worker."
+            description="Tell us about your need and our team will contact you soon."
+            />
+
+            <main>
+                <fieldset>
+                    <legend>Personal informations </legend>
+
+                    <Input name="name"  label="Full Name" />
+                    <Input name="company"  label="Company" />
+                    <Input name="adress"  label="Adress" />
+                    <Input name="tel"  label="Tel" />
+                    <Input name="email"  label="E-mail" />
+                    <Textarea name="bio" label="Give us a brief introduction of your need" />
+
+                </fieldset>
+
+                <footer>
+                    <p>
+                        <img src={warningIcon} alt="Important warning"/>
+                        Important! <br />
+                        Fill in all the registration fields.
+                    </p>
+                    <button type="button">
+                        Save registration
+                    </button>
+                </footer>
+            </main>
         </div>
     )
 }
